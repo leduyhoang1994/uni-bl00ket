c80d0c6a7f3e3a33964e7e7741c8af7eace89b94
HoangLQ <lequochoang21291@gmail.com>
2025-09-24 17:18:22 +0700

    add react game

---
+++ Frontend/src/games/cafe-game-react/screens/components/table-container/table-container.tsx
@@ -0,0 +1,53 @@
+import CafeGameStore from "@/games/stores/cafe-game-store/cafe-game-store";
+import Plate from "../plate/plate";
+import ButtonCafeGame from "@/games/cafe-game-react/components/button-cafe-game/button-cafe-game";
+import QuizStore from "@/stores/quiz-store/quiz-store";
+
+export default function TableContainer() {
+  const { setToggleQuizContainer, loadNewQuestion } = QuizStore();
+  const { setToggleVisitShop, cafeStocks, loadCafeShopItems } = CafeGameStore();
+
+  const doClickRestockFood = () => {
+    loadNewQuestion();
+    setToggleQuizContainer(true);
+  };
+
+  const doClickVisitShop = () => {
+    loadCafeShopItems();
+    setToggleVisitShop(true);
+  };
+
+  return (
+    <div className="cafe-game__table-container">
+      <div className="cafe-game__table-container-plate">
+        {cafeStocks.map((_, i) => {
+          let plateIndex;
+          if (i % 2 === 0) {
+            plateIndex = i / 2;
+          } else {
+            plateIndex = (i - 1) / 2 + 5;
+          }
+          const plateData = cafeStocks[plateIndex];
+          const plateLevel = plateData.currentIndexLevel;
+          const enabled = plateData.enabled;
+          const quantity = plateData.quantity;
+          return (
+            <div key={i}>
+              <Plate
+                enabled={enabled}
+                plateLevel={plateLevel}
+                quantity={quantity}
+                zIndex={cafeStocks.length - i}
+                plateData={plateData}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="cafe-game__table-container-cover-btn">
+        <ButtonCafeGame doClickBtn={doClickRestockFood} />
+        <ButtonCafeGame doClickBtn={doClickVisitShop} text="Visit Shop" />
+      </div>
+    </div>
+  )
+}
