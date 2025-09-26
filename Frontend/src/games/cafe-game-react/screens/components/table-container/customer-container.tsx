a7b5cd408f406f3855ea136d93bf9ee901e758bb
hoangld <hoangld@onschool.edu.vn>
2025-09-26 10:25:18 +0700

    Customer

---
+++ Frontend/src/games/cafe-game-react/screens/components/table-container/customer-container.tsx
@@ -0,0 +1,24 @@
+import CafeGameStore from "@/games/stores/cafe-game-store/cafe-game-store";
+import { useLayoutEffect } from "react";
+import Customer from "./customer";
+
+export default function CustomerContainer() {
+  const { loadCustomers, customers } = CafeGameStore();
+
+  useLayoutEffect(() => {
+    loadCustomers();
+  }, []);
+
+  return (
+    <div className="cafe-game__table-customer-container">
+      {customers.map((customer, position) => (
+        <div
+          key={`customer-${position + 1}`}
+          className="cafe-game__table-customer-slot"
+        >
+          <Customer position={position + 1} />
+        </div>
+      ))}
+    </div>
+  );
+}
--- Frontend/src/games/cafe-game-react/screens/customer-desk/customer-desk.tsx
+++ Frontend/src/games/cafe-game-react/screens/customer-desk/customer-desk.tsx
@@ -1,3 +1,4 @@
+import CustomerContainer from "../components/table-container/customer-container";
 import TableContainer from "../components/table-container/table-container";
 import WallContainer from "../components/wall-container/wall-container";
 
@@ -5,6 +6,7 @@ export default function CustomerDesk() {
   return (
     <div className="cafe-game">
       <WallContainer />
+      <CustomerContainer />
       <TableContainer />
     </div>
   )
